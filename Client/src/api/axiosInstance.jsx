5bdcf4d58d17333f76c3bc3e4b48106c24fc4eb2
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-06-15 02:14:59 +0600

    fontend auth setup

--- Client/src/api/axiosInstance.jsx
+++ Client/src/api/axiosInstance.jsx
@@ -9,4 +9,14 @@ const axiosInstance = axios.create({
   }
 });
 
+axiosInstance.interceptors.request.use((config)=>{
+  const accessToken = sessionStorage.getItem('accessToken');
+
+  if(accessToken){
+    config.headers.Authorization = `Bearer ${accessToken}`
+  }
+
+  return config;
+},(err)=> Promise.reject(err));
+
 export default axiosInstance;
