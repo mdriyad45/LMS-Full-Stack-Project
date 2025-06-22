56126d36a912c6d92c4984adcbd0877c7988faa6
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-06-22 13:00:42 +0600

    courseVideoModel

---
+++ Server/src/models/courseVideoMOdel.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const videoCourseSchema = new mongoose.Schema(
+  {
+    public_id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    title: {
+      type: String,
+      default: "Untitled Video",
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    width: {
+      type: Number,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+    },
+    format: {
+      type: String,
+      required: true,
+    },
+    bytes: {
+      type: Number,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+    streaming_urls: {
+      hls: { type: String },
+      dash: { type: String },
+      auto: { type: String },
+      qualities: {
+        low: { type: String },
+        medium: { type: String },
+        high: { type: String },
+      },
+    },
+    thumbnail: {
+      type: String,
+    },
+    uploadBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const CourseVideo = mongoose.model("CourseVideo", videoCourseSchema);
